0c4d61e62553a3db1f298f163fd197cc9941cee5
Kristy-Marc <kristymelo007@gmail.com>
2024-08-10 08:28:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let signinBtn = document.getElementById("signinBtn");
+let LoginBtn = document.getElementById("loginBtn");
+let companyName = document.getElementById("company");
+let Email = document.getElementById("email");
+let password = document.getElementById("pass");
+let title = document.getElementById("title");
+
+LoginBtn.onclick = function(){
+    companyName.style.maxHeight = "0";
+}
